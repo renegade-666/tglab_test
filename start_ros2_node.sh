259d9f3e7c4947fe2de7151f8e6ad45888a50fdc
cyan <omalenov@gmail.com>
2026-04-06 01:53:23 +0300

    gps node: moved yaml params file

--- start_ros2_node.sh
+++ start_ros2_node.sh
@@ -5,7 +5,7 @@ dirs=(*/)
 cd $current_dir
 echo "Entering workspace"
 cd dev_ws
-echo "Sourcing ROS 2 environment"
+echo "Sourcing ROS 2 environment"
 setup_path="/opt/ros/"
 setup_path+="${dirs[0]}"
 setup_path+="setup.bash"
@@ -13,4 +13,4 @@ source $setup_path
 echo "Sourcing overlay environment"
 . install/setup.bash
 echo "Launching gps node..."
-ros2 run py_pubsub gps_node __params:=./gps_node.yaml
+ros2 run py_pubsub gps_node __params:=./src/py_pubsub/gps_node.yaml
