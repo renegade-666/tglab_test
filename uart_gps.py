fd45c702d7f283cab04b21bff8f464a0f8d6bde8
cyan <omalenov@gmail.com>
2026-04-05 15:33:21 +0100

    uart gps: basic serial connection error handling

--- uart_gps.py
+++ uart_gps.py
@@ -21,29 +21,38 @@ def dumb_parse(fields):
 # Open the serial port
 with serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1) as ser:
     while True:
-        # Read a line from the serial port
-        gpgga_data = ser.readline().decode('ascii', errors='replace').strip()
-
-        # Check if the string starts with $GPGGA
-        if gpgga_data.startswith('$GPGGA'):
-            fields = gpgga_data.split(',')
-
-            if (len(fields) == NUM_GPGGA_FIELDS):
-
-                # Extracting relevant data
-                time, latitude, latitude_type, longitude, longitude_type, num_satellites = dumb_parse(fields)
-                if latitude < 0.0 or latitude > 90.0 or latitude_type != "N" and latitude_type != "S":
-                    print("Error: Invalid latitude")
-                    continue
-                if longitude < 0.0 or longitude > 180.0 or longitude_type != "E" and longitude_type != "W":
-                    print("Error: Invalid longitude")
-                    continue
-                if num_satellites <= 0:
-                    print("Error: Invalid number of satellites")
-                    continue
-
-                print(f"GPS: {latitude:.4f}° {latitude_type}, {longitude:.4f}° {longitude_type} | Супутники: {num_satellites}")
-            else:
-                print("Error: Incorrect number of fields in GPS data")
+        gpgga_data = ""
+        try:
+            # Read a line from the serial port
+            gpgga_data = ser.readline().decode('ascii', errors='replace').strip()
+        except serial.SerialException as e:
+            print("There is no new data from serial port")
+            exit()
+        except TypeError as e:
+            print("Disconnect of USB->UART occured")
+            ser.close()
+            exit()
         else:
-            print("Error: Not a NMEA GPGGA data format")
+            # Check if the string starts with $GPGGA
+            if gpgga_data.startswith('$GPGGA'):
+                fields = gpgga_data.split(',')
+
+                if (len(fields) == NUM_GPGGA_FIELDS):
+
+                    # Extracting relevant data
+                    time, latitude, latitude_type, longitude, longitude_type, num_satellites = dumb_parse(fields)
+                    if latitude < 0.0 or latitude > 90.0 or latitude_type != "N" and latitude_type != "S":
+                        print("Error: Invalid latitude")
+                        continue
+                    if longitude < 0.0 or longitude > 180.0 or longitude_type != "E" and longitude_type != "W":
+                        print("Error: Invalid longitude")
+                        continue
+                    if num_satellites <= 0:
+                        print("Error: Invalid number of satellites")
+                        continue
+
+                    print(f"GPS: {latitude:.4f}° {latitude_type}, {longitude:.4f}° {longitude_type} | Супутники: {num_satellites}")
+                else:
+                    print("Error: Incorrect number of fields in GPS data")
+            else:
+                print("Error: Not a NMEA GPGGA data format")
