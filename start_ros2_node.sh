d8f41ec84f3cc52f266f42d753877cf3bcc70dfa
cyan <omalenov@gmail.com>
2026-04-06 00:07:52 +0300

    gps node startup script additions

--- start_ros2_node.sh
+++ start_ros2_node.sh
@@ -1,8 +1,16 @@
+current_dir=$(pwd)
+cd /opt/ros
+dirs=(*/)
+
+cd $current_dir
 echo "Entering workspace"
 cd dev_ws
 echo "Sourcing ROS 2 environment"
-source /opt/ros/dashing/setup.bash
-echo "Sourcing the overlay"
+setup_path="/opt/ros/"
+setup_path+="${dirs[0]}"
+setup_path+="setup.bash"
+source $setup_path
+echo "Sourcing overlay environment"
 . install/setup.bash
 echo "Launching gps node..."
 ros2 run py_pubsub gps_node __params:=./gps_node.yaml
